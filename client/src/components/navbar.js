c5b9808679149b66f3e8d51516a37a8a10d0d353
Black Slash Creative <ross@blacksla.sh>
2023-08-27 10:31:36 -0700

    Adds logout

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -2,11 +2,22 @@ import { useContext } from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import { AppContext } from '../context';
+import { getAuth, signOut } from "firebase/auth";
 
 function NavBar(){
   // App Context
   const context = useContext(AppContext);
-  const { currentUser } = context;
+  const { currentUser, setCurrentUser } = context;
+
+  const logMeOut = () => {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      // Sign-out successful.
+      setCurrentUser(null);
+    }).catch((error) => {
+      // An error happened.
+    });
+  }
 
   return(
     <Navbar expand="lg" className="bg-body-tertiary">
@@ -16,12 +27,16 @@ function NavBar(){
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/register/">Register</Nav.Link>
-            <Nav.Link as={Link} to="/login/">Login</Nav.Link>
+            {currentUser ? (
+              <Nav.Link onClick={logMeOut} to="/logout/">Logout</Nav.Link>
+            ) : (
+              <Nav.Link as={Link} to="/login/">Login</Nav.Link>
+            )}
             <Nav.Link as={Link} to="/deposit/">Deposit</Nav.Link>
             <Nav.Link as={Link} to="/withdraw/">Withdraw</Nav.Link>
             <Nav.Link as={Link} to="/balance/">Balance</Nav.Link>
             <Nav.Link as={Link} to="/data/">AllData</Nav.Link>
-            {currentUser && <a class="nav-link flex-right">{currentUser.email}</a>}
+            {currentUser && <a className="nav-link flex-right">{currentUser.email}</a>}
           </Nav>
         </Navbar.Collapse>
       </Container>
