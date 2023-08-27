67fddd40538db28d4b5f40046beb5da332a56f30
Black Slash Creative <ross@blacksla.sh>
2023-08-26 23:19:30 -0700

    Adds app context to login page

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,4 +1,13 @@
+import { useContext } from 'react';
+//import Card from './card';
+import { AppContext } from '../context';
+
 function LoginForm(){
+  
+  const context = useContext(AppContext);
+  console.log(`context: ${JSON.stringify(context)}`);
+  const { currentUser, setCurrentUser } = context;
+
   return(
     <>
     <h1>Login</h1>
